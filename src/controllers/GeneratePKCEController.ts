c08b303c4e725feeb468accc1c2c64de78d02d44
Nil-ton <nilton.oliveira0305@gmail.com>
2024-08-02 09:24:24 -0300

    remove exemple

--- src/controllers/GeneratePKCEController.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyReply, FastifyRequest } from 'fastify';
-import { GeneratePKCEUseCase } from '../useCases/GeneratePKCEUseCase'
-
-export function GeneratePKCEController(req: FastifyRequest, reply: FastifyReply) {
-    const generatepkceUseCase = new GeneratePKCEUseCase();
-    return generatepkceUseCase.execute();
-}
--- src/useCases/GeneratePKCEUseCase.ts
+++
@@ -1,3 +0,0 @@
-export class GeneratePKCEUseCase {
-    async execute() {}
-}
